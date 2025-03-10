6dab011078bc1471606fd8744ef38750c6630205
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2025-03-10 12:28:27 +0000

    Reveett

--- src/open_r1/sft.py
+++ src/open_r1/sft.py
@@ -121,7 +121,7 @@ def main(script_args, training_args, model_args):
         trust_remote_code=model_args.trust_remote_code,
         attn_implementation=model_args.attn_implementation,
         torch_dtype=torch_dtype,
-        use_cache=False if training_args.gradient_checkpointing else True,
+        use_cache=False, # if training_args.gradient_checkpointing else True,
         device_map=get_kbit_device_map() if quantization_config is not None else None,
         quantization_config=quantization_config,
     )
